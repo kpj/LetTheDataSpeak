fb8091f44ee91a9d0998c8f32f2a8bd9cc2c2d50
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2014-02-26 17:28:57 +0100

    Added simple visualization

--- frontend/js/playtest.js
+++ frontend/js/playtest.js
@@ -1,34 +1,56 @@
 $(function(){
 
-	var interval = undefined; 
-	var sequence = undefined; 
+	var interval = undefined;
+	var sequence = undefined;
+
+	var wave = undefined;
+
+	var frequencyPlot = undefined;
+	var plotInterval = undefined;
 
 
-	var iterate = function(){
+	var iterate = function() {
 		//get something here
 
 		var next = sequence.nextInteger(true);
 		
 		//lets play something
-		T("sin", {freq: T("pulse", {freq: next*10, add: 880, mul: 20}).kr()})
-		.on("ended", function() {
-			this.pause();
-		}).bang().play();
+		if(typeof wave == "undefined") {
+			wave = T("sin", {freq: T("pulse", {freq: next*10, add: 880, mul: 20}).kr()})
+			.on("ended", function() {
+				this.pause();
+			}).bang().play();
+
+			frequencyPlot = T("fft").listen(wave);
+		} else {
+			wave.set({freq: T("pulse", {freq: next*10, add: 880, mul: 20}).kr()});
+		}
 	}
 
-	var play = function(){
+	var play = function() {
 		stop(); 
-		sequence = new DnaSequence($("#data").val()); 
-		interval = setInterval(iterate, 800); 
+		sequence = new DnaSequence($("#data").val());
+
+		interval = setInterval(iterate, 500);
+		plotInterval = setInterval(function() {
+			if(typeof frequencyPlot !== "undefined") {
+				frequencyPlot.plot({target: $("#canvas")[0]});
+			}
+		}, 50);
 	};
 
-	var stop = function(){
+	var stop = function() {
 		if(typeof interval !== "undefined"){
-			clearInterval(interval); 
-			interval = undefined; 
+			clearInterval(interval);
+			clearInterval(plotInterval);
+
+			interval = undefined;
+			wave = undefined;
+			frequencyPlot = undefined;
+			plotInterval = undefined;
 		}
 	};
 
-	$("#play").click(function(){play(); return false; });
-	$("#stop").click(function(){stop(); return false; })
+	$("#play").click(function() {play(); return false; });
+	$("#stop").click(function() {stop(); return false; })
 });
