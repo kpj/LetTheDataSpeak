877f8e449b7be00a9f033df6d7a089b32796c1de
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2014-03-01 16:03:56 +0100

    Added piano support

--- frontend/js/music_generators.js
+++ frontend/js/music_generators.js
@@ -1,4 +1,4 @@
-SimpleWaves = function() {
+var SimpleWaves = function() {
 	var me = this;
 	this._wave = undefined;
 
@@ -20,5 +20,46 @@ SimpleWaves = function() {
 
 	this.isPlaying = function() {
 		return typeof this._wave !== "undefined";
+	}
+}
+
+var MidiPlayer = function() {
+	var me = this;
+	this._note = undefined;
+
+	// TODO: make this nice and pretty
+	var instruments = {
+		"acoustic_grand_piano": 0,
+		"acoustic_guitar_nylon": 24,
+		"electric_bass_finger": 33
+	};
+	MIDI.loadPlugin({
+	soundfontUrl: "./lib/midi/soundfont/",
+	instruments: Object.keys(instruments),
+	callback: function() {
+			console.log("Loaded MIDI"); 
+
+			MIDI.setVolume(0, 127);
+			MIDI.programChange(0, 0);
+
+			// enable play button
+			$('#play').removeAttr('disabled');
+		}
+	});
+
+	this.setNote = function(note) {
+		me._note = 21 + (note % 75);
+	}
+
+	this.playNote = function() {
+		MIDI.noteOn(0, me._note, 100);
+	};
+
+	this.stop = function() {
+		this._note = undefined;
+	}
+
+	this.isPlaying = function() {
+		return typeof this._note !== "undefined";
 	}
 }
--- frontend/js/playtest.js
+++ frontend/js/playtest.js
@@ -1,19 +1,22 @@
 $(function(){
 
-	var musicGenerator = new SimpleWaves(); // to alter music generation simply exchange this class
+	var musicGenerator = new MidiPlayer(); // to alter music generation simply exchange this class
 
 	var interval = undefined;
 	var sequence = undefined;
+
 
 	var iterate = function() {
 		var next = sequence.nextInteger(true);
 		
 		//lets play something
-		if(!musicGenerator.isPlaying()) {
+		/*if(!musicGenerator.isPlaying()) {
 			musicGenerator.newTone({'multi': next});
 		} else {
 			musicGenerator.setAttributes({freq: T("pulse", {freq: next*10, add: 880, mul: 20}).kr()});
-		}
+		}*/
+		musicGenerator.setNote(next);
+		musicGenerator.playNote();
 	}
 
 	var play = function() {
