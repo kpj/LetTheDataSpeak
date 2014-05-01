5b87b96e90623c889b486f4a02b29912e631d6d6
Tom Wiesing <tkw01536@gmail.com>
2014-05-01 13:16:42 +0200

    Some BS Fixes

--- js/master.js
+++ js/master.js
@@ -3,7 +3,7 @@ $(function(){
 	//code in the master
 	var addTrack = function(){
 		$("#content").append(
-			'<iframe class="row" src="./components/player.html">Please enable iframes. </iframe>', 
+			'<iframe class="line" src="./components/player.html">Please enable iframes. </iframe>', 
 			'<hr />'
 		).css("margin-top", - $("#content").height() / 2); 
 	}
